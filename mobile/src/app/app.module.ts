61759030e5d14e013c9a1091c6c79e1a4c9d596a
Patrick Oberbichler <patrick.oberbichler@gmail.com>
2022-09-11 13:22:17 +0200

    remove dark mode

--- mobile/src/app/app.module.ts
+++ mobile/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicStorageModule } from "@ionic/storage-angular";
 
+import { CacheModule } from "ionic-cache";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SessionData } from './providers/session-data';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(),
+    CacheModule.forRoot(),
+    IonicStorageModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    SessionData
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
