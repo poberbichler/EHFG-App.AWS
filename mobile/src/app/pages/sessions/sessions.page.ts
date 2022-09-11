29d711971be95c89434e8d5ce2a4c384d190bb3c
Patrick Oberbichler <patrick.oberbichler@gmail.com>
2022-09-11 17:05:24 +0200

    display session overview

--- mobile/src/app/pages/sessions/sessions.page.ts
+++ mobile/src/app/pages/sessions/sessions.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ConferenceDay } from 'src/app/data/conferenceday';
+import { SessionData } from 'src/app/providers/session-data';
 
 @Component({
   selector: 'app-sessions',
@@ -6,7 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['sessions.page.scss']
 })
 export class SessionsPage {
+  dayMap: Map<string, ConferenceDay>;
+  days: string[];
 
-  constructor() {}
+  constructor(
+    private sessionData: SessionData,
+  ) {}
 
+  ionViewDidEnter() {
+    this.sessionData.getSessions().subscribe(data => {
+      this.dayMap = data;
+      this.days = Object.keys(data);
+    });
+  }
 }
